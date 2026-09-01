f12aed11bac53886e4460eab191fbb97113e61ff
raanvixtechnologies <raanvixtechnologies@gmail.com>
2026-09-01 10:21:03 +0530

    Update StoryChapters.tsx
    text modified

--- src/sections/StoryChapters.tsx
+++ src/sections/StoryChapters.tsx
@@ -102,7 +102,7 @@ export const ChapterSeven = () => (
       transition={{ duration: 1.5, delay: 2.5 }}
       className="text-lg sm:text-xl md:text-2xl font-handwriting text-pastel-pink mt-4 px-4"
     >
-      anduke honey ane donkey ni love chesa
+      anduke honey papa ni love chesa
     </motion.p>
   </ChapterLayout>
 );
